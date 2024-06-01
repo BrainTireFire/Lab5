3335f4fa54e17f86c0c69986c6fd623b60600f9b
Maciej Kawęcki <skimiskim12@gmail.com>
2024-06-01 14:23:04 +0200

    fixed

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pickle
 import numpy as np
 from datetime import datetime
 
-
 startTime = datetime.now()
 # import znanych nam bibliotek
 
@@ -12,7 +11,7 @@ model = pickle.load(open(filename, 'rb'))
 # otwieramy wcześniej wytrenowany model
 
 sex_d = {0: "Kobieta", 1: "Mężczyzna"}
-pclass_d = {0: "Pierwsza", 1: "Druga", 2: "Trzecia"}
+pclass_d = {1: "Pierwsza", 2: "Druga", 3: "Trzecia"}
 embarked_d = {0: "Cherbourg", 1: "Queenstown", 2: "Southampton"}
 
 
@@ -23,36 +22,26 @@ def main():
     left, right = st.columns(2)
     prediction = st.container()
 
-    st.image(
-        "https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG")
-
+    
     with overview:
         st.title("Czy przeżyłbyś katastrofę?")
+        st.image(
+        "https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG")
 
     with left:
-        sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
-        pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
-        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2,
-                                  format_func=lambda x: embarked_d[x])
+        sex_radio = st.radio("Płeć:", list(sex_d.keys()), format_func=lambda x: sex_d[x])
+        pclass_radio = st.radio("Klasa:", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
+        embarked_radio = st.radio("Port zaokrętowania:", list(embarked_d.keys()), index=2, format_func=lambda x: embarked_d[x])
 
     with right:
-        age_slider = st.slider("Wiek", value=50, min_value=1, max_value=100)
-        sibsp_slider = st.slider("# Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
-        parch_slider = st.slider("# Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
-        fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=10)
+        age_slider = st.slider("Wiek:", min_value=1, max_value=100)
+        fare_slider = st.slider("Cena biletu:", min_value=0, max_value=500, step=10)
+        sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera:", min_value=0, max_value=8)
+        parch_slider = st.slider("Liczba rodziców i/lub dzieci:", min_value=0, max_value=6)
 
     if st.button("Przewiduj"):
-        data = np.array([
-                [
-                    pclass_radio, 
-                    sex_radio, 
-                    age_slider, 
-                    sibsp_slider, 
-                    parch_slider,
-                    fare_slider, 
-                    embarked_radio
-                ]
-            ])
+        data = np.array([[pclass_radio, age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio, sex_radio]])
+
         survival = model.predict(data)
         s_confidence = model.predict_proba(data)
 
